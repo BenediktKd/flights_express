d445f31cc14fbffeabca523eeb1df520538fb1c5
BenediktKd <benedikt@uc.cl>
2023-11-14 17:08:28 -0300

    feat: combine flights

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const { listFiles, downloadFlightData, downloadFile , processAircraftsXML, processAirportsCSV,
-processPassengersYAML, processTicketsCSV } = require('./utils/storage');
+      processPassengersYAML, processTicketsCSV } = require('./utils/storage');
 const path = require('path');
+const fs = require('fs').promises;
 
 const aircraftsXMLPath = 'aircrafts.xml';
 const airportsCSVFilename = 'airports.csv';
@@ -80,7 +81,40 @@ async function downloadAndProcessTicketsCSV() {
   }
 }
 
+async function readFlightData(year, month) {
+  const monthFolderPath = path.join('data', 'flights', year.toString(), month.toString().padStart(2, '0'), 'flight_data.json');
+  try {
+    const data = await fs.readFile(monthFolderPath, 'utf-8');
+    const flights = JSON.parse(data);
+    // Añade año y mes a cada vuelo
+    return flights.map(flight => ({ ...flight, year, month }));
+  } catch (error) {
+    console.error(`Error al leer los datos de vuelo para ${year}-${month}:`, error);
+    return []; // Devuelve un array vacío si no se pudo leer el archivo
+  }
+}
 
+async function combineAllFlightData() {
+  let allFlights = [];
+
+  try {
+    const years = await fs.readdir(path.join('data', 'flights')); // Obtener todos los años disponibles de forma asíncrona
+    for (const year of years) {
+      const months = await fs.readdir(path.join('data', 'flights', year)); // Obtener todos los meses para cada año de forma asíncrona
+      for (const month of months) {
+        const flightsForMonth = await readFlightData(year, month);
+        allFlights = allFlights.concat(flightsForMonth); // Agrega los vuelos al array principal
+      }
+    }
+  
+    // Una vez que todos los vuelos se han recopilado, escribe el array en un nuevo archivo JSON
+    const combinedFilePath = path.join('data', 'combined_flights.json');
+    await fs.writeFile(combinedFilePath, JSON.stringify(allFlights, null, 2), 'utf-8');
+    console.log(`Todos los vuelos han sido combinados en ${combinedFilePath}`);
+  } catch (error) {
+    console.error('Error al combinar los datos de vuelo:', error);
+  }
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -88,11 +122,15 @@ app.listen(PORT, () => {
 });
 
 // listFiles();
-downloadFlightData();
-downloadAndProcessAircrafts();
-downloadAndProcessAirportsCSV();
-downloadAndProcessPassengersYAML();
-downloadAndProcessTicketsCSV();
+// downloadFlightData();
+// downloadAndProcessAircrafts();
+// downloadAndProcessAirportsCSV();
+// downloadAndProcessPassengersYAML();
+// downloadAndProcessTicketsCSV();
+
+
+combineAllFlightData();
+
 
 
 
